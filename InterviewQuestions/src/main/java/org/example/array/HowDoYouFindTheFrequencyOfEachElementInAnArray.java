964af12cf6de0bc2223975a42d44d14760c8fbac
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2025-01-05 05:51:37 +0530

    With java notes

--- InterviewQuestions/src/main/java/org/example/array/HowDoYouFindTheFrequencyOfEachElementInAnArray.java
+++ InterviewQuestions/src/main/java/org/example/array/HowDoYouFindTheFrequencyOfEachElementInAnArray.java
@@ -8,39 +8,23 @@ import java.util.Map;
 public class HowDoYouFindTheFrequencyOfEachElementInAnArray {
 
 
-	  public static String removeDuplicates(String strin) {
-          if (strin == null || strin.length() == 0) {
-              return null;
-          }
 
-          char[] str=strin.toCharArray();
-          
-          int length = str.length;
-//We initialize variables: `length` to store the length of the string and `tail` to keep track of the position where the next unique character should be placed.
-          
-          int tail = 1;
-
-          for (int i = 1; i < length; i++) {
-              int j;
-              for (j = 0; j < tail; j++) {
-                  if (str[i] == str[j]) {
-                      break;
-                  }
-              }
-//If the inner loop completes without finding a duplicate, we copy the character to position `tail` and increment `tail`.
-              if (j == tail) {
-                  str[tail] = str[i];
-                  tail++;
-              }
-          }
-
-          return new String(str,0,tail);
-      }
-
-      public static void main(String[] args) {
-          String str = "Hello World";
-         
-          System.out.println("String with duplicates removed: " +  removeDuplicates(str));
-      }
-  
+        public static void main(String[] args) {
+            int[] array = {1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5};
+    
+            // Create a HashMap to store frequencies
+            HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+    
+            // Iterate through the array
+            for (int num : array) {
+                // If the number is already in the map, increment its count
+                frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+            }
+    
+            // Print the frequencies
+            for (HashMap.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+                System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
+            }
+        }
     }
+    
